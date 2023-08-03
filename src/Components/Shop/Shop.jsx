38126f7b3bcc4f0e1093e1c111f1a3f9f628bfa4
rojinaeva <rojina.eva.cse@gmail.com>
2023-08-03 13:06:39 +0600

    cart added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,22 +4,29 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products,setProducts]=useState([]);
+    const[cart,setCart]=useState([]);
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    const handleAddToCart=(product)=>{
+       const newCart=[...cart,product];
+       setCart(newCart);
+    }
     return (
         <div className='shop-container'>
            <div className="products-container">
             {
                 products.map(product=><Product
                 key={product.id}
-                product={product}></Product>)
+                product={product}
+                handleAddToCart={handleAddToCart}></Product>)
             }
            </div>
            <div className="cart-container">
               <h3>cart summary</h3>
+              <p>selected items: {cart.length}</p>
            </div>
         </div>
     );
